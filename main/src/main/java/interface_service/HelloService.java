c62a9ebddc44b9ae13e0c80c47f27338064f0ba0
Jonnathan23 <jsaquicela26@gmail.com>
2024-12-11 18:57:09 -0500

    conexion con el cliente

--- main/src/main/java/interface_service/HelloService.java
+++ main/src/main/java/interface_service/HelloService.java
@@ -1,7 +1,8 @@
 package interface_service;
 
 import java.rmi.RemoteException;
+import java.rmi.Remote;
 
-public interface HelloService {
+public interface HelloService extends Remote {
 	String sayHello(String name) throws RemoteException;
 }
--- main/src/main/java/practica_rcp/main/Server.java
+++ main/src/main/java/practica_rcp/main/Server.java
@@ -3,24 +3,31 @@ package practica_rcp.main;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+
 import Service.HelloServiceImpl;
 import interface_service.HelloService;
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
 
+@Singleton
+@Startup
 public class Server {
-
-	public static void main(String[] args) {
 	
+	@PostConstruct
+	public void init() {
 		try {
 			HelloService service = new HelloServiceImpl();
 			
-			Registry registry = LocateRegistry.createRegistry(1099);
+			Registry registry = LocateRegistry.createRegistry(8081);
 			
-			 registry.rebind("HelloService", service);
+			 registry.rebind("Registro", service);
+			 System.out.println("\n |----------------| |Servidor RMI iniciado y listo para aceptar llamadas. | |----------------|");
 			 
-			 System.out.println("Servidor RMI iniciado y listo para aceptar llamadas.");
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("|----------------| | ERROR Servidor | |----------------| |");
+			System.out.println(e);
 		}
-		
-	}	
+	}
+	
 }
